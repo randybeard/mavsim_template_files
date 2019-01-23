acc8f57a5584503f7464a238dd7c4773bb3c0433
Randy Beard <beard@byu.edu>
2019-01-23 09:53:10 -0700

    Removed old simulink template files and added revised templates for chapters 2, 3, 4

---
+++ mavsim_simulink/chap3/mav_dynamics.m
@@ -0,0 +1,211 @@
+function [sys,x0,str,ts,simStateCompliance] = mav_dynamics(t,x,u,flag,P)
+
+switch flag
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P);
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1
+    sys=mdlDerivatives(t,x,u,P);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3
+    sys=mdlOutputs(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+% end sfuntmpl
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P)
+
+%
+% call simsizes for a sizes structure, fill it in and convert it to a
+% sizes array.
+%
+% Note that in this example, the values are hard coded.  This is not a
+% recommended practice as the characteristics of the block are typically
+% defined by the S-function parameters.
+%
+sizes = simsizes;
+
+sizes.NumContStates  = 12;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 12;
+sizes.NumInputs      = 6;
+sizes.DirFeedthrough = 0;
+sizes.NumSampleTimes = 1;   % at least one sample time is needed
+
+sys = simsizes(sizes);
+
+%
+% initialize the initial conditions
+%
+x0  = [...
+    P.pn0;...
+    P.pe0;...
+    P.pd0;...
+    P.u0;...
+    P.v0;...
+    P.w0;...
+    P.phi0;...
+    P.theta0;...
+    P.psi0;...
+    P.p0;...
+    P.q0;...
+    P.r0;...
+    ];
+
+%
+% str is always an empty matrix
+%
+str = [];
+
+%
+% initialize the array of sample times
+%
+ts  = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,uu, P)
+
+    pn    = x(1);
+    pe    = x(2);
+    pe    = x(3);
+    u     = x(4);
+    v     = x(5);
+    w     = x(6);
+    phi   = x(7);
+    theta = x(8);
+    psi   = x(9);
+    p     = x(10);
+    q     = x(11);
+    r     = x(12);
+    fx    = uu(1);
+    fy    = uu(2);
+    fz    = uu(3);
+    ell   = uu(4);
+    m     = uu(5);
+    n     = uu(6);
+    
+  
+    pndot = 
+    pedot = 
+    pddot = 
+    udot = 
+    vdot = 
+    wdot = 
+    phidot = 
+    thetadot = 
+    psidot = 
+    pdot = 
+    qdot = 
+    rdot = 
+
+sys = [pndot; pedot; pddot; udot; vdot; wdot; phidot; thetadot; psidot; pdot; qdot; rdot];
+
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+
+sys = [];
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u)
+
+sys = x;
+
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
---
+++ mavsim_simulink/chap3/param_chap3.m
@@ -0,0 +1,23 @@
+P.gravity = 
+   
+%physical parameters of airframe
+P.mass = 
+P.Jx   = 
+P.Jy   = 
+P.Jz   = 
+P.Jxz  = 
+
+% initial conditions
+P.pn0    =   % initial North position
+P.pe0    =   % initial East position
+P.pd0    =   % initial Down position (negative altitude)
+P.u0     =   % initial velocity along body x-axis
+P.v0     =   % initial velocity along body y-axis
+P.w0     =   % initial velocity along body z-axis
+P.phi0   =   % initial roll angle
+P.theta0 =   % initial pitch angle
+P.psi0   =   % initial yaw angle
+P.p0     =   % initial body frame roll rate
+P.q0     =   % initial body frame pitch rate
+P.r0     =   % initial body frame yaw rate
+
