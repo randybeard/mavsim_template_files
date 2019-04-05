310454791236a50e17a583d49488b83a171b3a98
Brady Moon <bradygmoon@gmail.com>
2019-04-05 09:49:13 -0600

    Added template for planRRT

--- mavsim_python/chap12/path_planner.py
+++ mavsim_python/chap12/path_planner.py
@@ -47,8 +47,30 @@ class path_planner:
                              np.radians(45),
                              np.radians(45),
                              np.radians(-135)]])
-        # elif planner_flag == 3:
-        #
+        elif planner_flag == 3:
+            self.waypoints.type = 'fillet'
+            self.waypoints.num_waypoints = 0
+            Va = 25
+            # current configuration vector format: N, E, D, Va
+            wpp_start = np.array([state.n,
+                                  state.e,
+                                  -state.h,
+                                  state.Va])
+            if np.linalg.norm(np.array([state.n, state.e, -state.h])-np.array([map.city_width, map.city_width, -state.h])) == 0:
+                wpp_end = np.array([0,
+                                    0,
+                                    -state.h,
+                                    Va])
+            else:
+                wpp_end = np.array([map.city_width,
+                                    map.city_width,
+                                    -state.h,
+                                    Va])
+
+            waypoints = self.rrt.planPath(wpp_start, wpp_end, map)
+            self.waypoints.ned = waypoints.ned
+            self.waypoints.airspeed = waypoints.airspeed
+            self.waypoints.num_waypoints = waypoints.num_waypoints
         # elif planner_flag == 4:
 
         else:
